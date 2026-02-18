3299c2610c737ccdfeca92749bd513ead719f2ad
mayurpatil0906 <mayurpatill0906@gmail.com>
2026-02-18 15:22:51 +0530

    Coded UC3 to print OOPS using Stingjoin

--- Message.java
+++ Message.java
@@ -1,19 +1,19 @@
 /*
 @author developer
-@version 2.0
+@version 3.0
 */
 
 class Message
 {
 	public static void main(String[]args)
 	{
-		System.out.println("   ***    "+" "+"   ***    "+" "+" ******  "+" "+"  ******   ");
-		System.out.println("**     ** "+" "+"**     ** "+" "+" **   ** "+" "+" **        ");  
-		System.out.println("**     ** "+" "+"**     ** "+" "+" **   ** "+" "+"**         ");  
-		System.out.println("**     ** "+" "+"**     ** "+" "+" **  **  "+" "+" ******    ");  
-		System.out.println("**     ** "+" "+"**     ** "+" "+" **      "+" "+"      **   ");  
-		System.out.println("**     ** "+" "+"**     ** "+" "+" **      "+" "+"       **  ");  
-		System.out.println("   ***    "+" "+"   ***    "+" "+" **      "+" "+"  ******  ");  
+		System.out.println(String.join(" # ","   ***     ","   ***    "," ******  ","  ******   "));
+		System.out.println(String.join(" # ","**     **  ","**     ** "," **   ** "," **        "));  
+		System.out.println(String.join(" # ","**     **  ","**     ** "," **   ** ","**         "));  
+		System.out.println(String.join(" # ","**     **  ","**     ** "," **  **  "," ******    "));  
+		System.out.println(String.join(" # ","**     **  ","**     ** "," **      ","      **   "));  
+		System.out.println(String.join(" # ","**     **  ","**     ** "," **      ","       **  "));  
+		System.out.println(String.join(" # ","   ***     ","   ***    "," **      ","  ******   "));  
 		
 	}
 }
