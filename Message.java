190d058fe400b10f53d78aaad8caa7889b3cbdb1
mayurpatil0906 <mayurpatill0906@gmail.com>
2026-02-18 16:08:10 +0530

    Coded UC5 to print string array

--- Message.java
+++ Message.java
@@ -7,19 +7,20 @@ class Message
 {
 	public static void main(String[]args)
 	{
-		String[] lines=new String[7];
-		lines[0]=String.join(" # ","   ***     ","   ***    "," ******  ","  ******   ");
-		lines[1]=String.join(" # ","**     **  ","**     ** "," **   ** "," **        ");  
-		lines[2]=String.join(" # ","**     **  ","**     ** "," **   ** ","**         ");  
-		lines[3]=String.join(" # ","**     **  ","**     ** "," **  **  "," ******    ");  
-		lines[4]=String.join(" # ","**     **  ","**     ** "," **      ","      **   ");  
-		lines[5]=String.join(" # ","**     **  ","**     ** "," **      ","       **  ");  
-		lines[6]=String.join(" # ","   ***     ","   ***    "," **      ","  ******   ");  
+		String names[]={
+			String.join(" # ","   ***     ","   ***    "," ******  ","  ******   "),
+			String.join(" # ","**     **  ","**     ** "," **   ** "," **        "),  
+			String.join(" # ","**     **  ","**     ** "," **   ** ","**         "),  
+			String.join(" # ","**     **  ","**     ** "," **  **  "," ******    "),  
+			String.join(" # ","**     **  ","**     ** "," **      ","      **   "),  
+			String.join(" # ","**     **  ","**     ** "," **      ","       **  "),  
+			String.join(" # ","   ***     ","   ***    "," **      ","  ******   ")  
+		};
 		
-		for (String line :lines)
-		{
-				System.out.println(line);
+		for (String name:names){
+			System.out.println(name);
 		}
+		
 		
 	}
 }
