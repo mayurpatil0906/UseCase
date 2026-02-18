270df5b4693711e6cef9d3c11c94091e35a8f79b
mayurpatil0906 <mayurpatill0906@gmail.com>
2026-02-18 15:15:13 +0530

    Coded UC2 to print banner app

--- Message.java
+++ Message.java
@@ -1,12 +1,19 @@
 /*
 @author developer
-@version 1.0
+@version 2.0
 */
 
 class Message
 {
-	public static void main(String[]args){
-	
-		System.out.print("OPPS");
+	public static void main(String[]args)
+	{
+		System.out.println("   ***    "+" "+"   ***    "+" "+" ******  "+" "+"  ******   ");
+		System.out.println("**     ** "+" "+"**     ** "+" "+" **   ** "+" "+" **        ");  
+		System.out.println("**     ** "+" "+"**     ** "+" "+" **   ** "+" "+"**         ");  
+		System.out.println("**     ** "+" "+"**     ** "+" "+" **  **  "+" "+" ******    ");  
+		System.out.println("**     ** "+" "+"**     ** "+" "+" **      "+" "+"      **   ");  
+		System.out.println("**     ** "+" "+"**     ** "+" "+" **      "+" "+"       **  ");  
+		System.out.println("   ***    "+" "+"   ***    "+" "+" **      "+" "+"  ******  ");  
+		
 	}
 }
