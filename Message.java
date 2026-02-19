b4e2b67de71de43f0a10e9d6fd70e7e6aeb74379
mayurpatil0906 <mayurpatill0906@gmail.com>
2026-02-19 09:52:14 +0530

    Coded UC5 to print OOPS to Console

--- Message.java
+++ Message.java
@@ -1,34 +1,26 @@
 /*
 @author developer
-@version 4.0
+@version 3.0
 */
 
 class Message
 {
 	public static void main(String[]args)
 	{
-		//Creating a array to store the oops patterns
-		String names[]=getpattern();
-			
+		String names[]={
+			String.join(" # ","   ***     ","   ***    "," ******  ","  ******   "),
+			String.join(" # ","**     **  ","**     ** "," **   ** "," **        "),  
+			String.join(" # ","**     **  ","**     ** "," **   ** ","**         "),  
+			String.join(" # ","**     **  ","**     ** "," **  **  "," ******    "),  
+			String.join(" # ","**     **  ","**     ** "," **      ","      **   "),  
+			String.join(" # ","**     **  ","**     ** "," **      ","       **  "),  
+			String.join(" # ","   ***     ","   ***    "," **      ","  ******   ")  
+		};
 		
 		for (String name:names){
 			System.out.println(name);
 		}
+		
+		
 	}
-		
-		public static String[] getpattern(){
-			return new String[] {
-				"  ***  ",
-				"**   **",
-				"**   **",
-				"**   **",
-				"**   **",
-				"  ***  "
-			};
-		}
-		
-		
-		
-		
-	
 }
